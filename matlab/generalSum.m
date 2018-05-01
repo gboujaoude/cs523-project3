3b6b49a93136f18710cc28bc5f397622dcde73b4
George Elie Boujaoude <gboujaoude@unm.edu>
2018-04-30 21:01:46 -0600

    Plots now include cell, infected, and lymphocyte count

--- matlab/generalSum.m
+++ matlab/generalSum.m
@@ -22,7 +22,10 @@ for group=1:length(configGroups)
     
     % Iterate virus-over-time file in each subdirectory
     virus_avg = zeros(1,(virusMax-virusMin)+1);
-    virus_file_counter = 0;
+    health_avg = zeros(1,(virusMax-virusMin)+1);
+    infected_avg = zeros(1,(virusMax-virusMin)+1);
+    lympho_avg = zeros(1,(virusMax-virusMin)+1);
+    file_counter = 0;
     for run=1:length(subDirs)
         subStr = subDirs{run};
         cd(subStr);
@@ -31,26 +34,53 @@ for group=1:length(configGroups)
         virus_over_time = virus_over_time';
         virus_over_time = virus_over_time(1,virusMin:virusMax);
         
-        virus_avg = virus_avg + virus_over_time;
-        virus_file_counter = virus_file_counter + 1;
+        health_over_time = csvread('healthy-over-time.csv');
+        health_over_time = health_over_time';
+        health_over_time = health_over_time(1,virusMin:virusMax);
         
+        infected_over_time = csvread('infected-over-time.csv');
+        infected_over_time = infected_over_time';
+        infected_over_time = infected_over_time(1,virusMin:virusMax);
+        
+        lympho_over_time = csvread('lymphocytes-over-time.csv');
+        lympho_over_time = lympho_over_time';
+        lympho_over_time = lympho_over_time(1,virusMin:virusMax);
+        
+        virus_avg = virus_avg + virus_over_time;
+        health_avg = health_avg + health_over_time;
+        infected_avg = infected_avg + infected_over_time;
+        lympho_avg = lympho_avg + lympho_over_time;
+        
+        file_counter = file_counter + 1;
         cd ../
     end
     cd ../
     
     % Make figure
-    virus_avg = virus_avg./virus_file_counter;
+    virus_avg = virus_avg./file_counter;
+    health_avg = health_avg./file_counter;
+    infected_avg = infected_avg./file_counter;
+    lympho_avg = lympho_avg./file_counter;
+    
     my_png = figure;
-    plot(floor(linspace(virusMin,virusMax,(virusMax-virusMin)+1)),virus_avg);
+    plot(floor(linspace(virusMin,virusMax,(virusMax-virusMin)+1)),virus_avg,'-r');
     hold on;
+    plot(floor(linspace(virusMin,virusMax,(virusMax-virusMin)+1)),health_avg,'-b');
+    plot(floor(linspace(virusMin,virusMax,(virusMax-virusMin)+1)),infected_avg,'-k');
+    plot(floor(linspace(virusMin,virusMax,(virusMax-virusMin)+1)),lympho_avg,'-g');
+        
     xlim([virusMin virusMax]);
-    ylim([0 6000]);
-    newTitle = strcat('Average Virus-Over-Time:',str);
+    ylim([0 3000]);
+    
+    newTitle = strcat('Immune response over time:',str);
     title(newTitle,'Interpreter','none');
     xlabel('Timestep','FontSize',12);
-    ylabel('Viruses','FontSize',12);
+    ylabel('Amount','FontSize',12);
+    legend('viruses','healthy cells','infected cells','lymphocytes');
+    
     hold off;
-    imageFilename = strcat('virus-over-time-',str);
+    
+    imageFilename = strcat(str);
     imageDir = strcat('../matlab/plots/',imageFilename);
     saveas(my_png,imageDir,'png');
 end
